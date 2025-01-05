c3c6c7e43787aa57dabddba87d39931a3fd2d87f
Franck kiemde <kiemdefranck5@gmail.com>
2025-01-05 08:52:21 +0100

    :tada:

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Logo from '../components/Logo';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A propos</h1>
+            <br />
+
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores ipsam fugit quod molestiae tenetur quo sequi ad eum nobis esse, laboriosam quasi voluptas eos enim nihil sed quae est? Ratione laborum cumque impedit, hic aliquid est velit deserunt ab? Enim illo, at aut, error hic iure reprehenderit doloremque rem saepe incidunt excepturi obcaecati odit. Impedit similique corrupti incidunt, molestias animi labore, aperiam corporis quas rerum repellendus, maiores exercitationem distinctio quibusdam voluptate! Sit aperiam culpa quod expedita sequi quidem repellendus voluptatem, voluptates animi veritatis iusto minima accusantium eius labore hic repudiandae, illum fugit esse? Natus facilis officiis, ea corporis vel architecto.</p>
+        <br />
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum cumque aperiam rem amet veritatis, sint maxime nihil eveniet similique magni? Alias distinctio sunt quae cupiditate placeat dicta corrupti unde eligendi quia omnis. Modi officiis expedita aliquid accusamus deserunt sit necessitatibus maxime consectetur vel, nam magni velit pariatur accusantium, atque animi tenetur quaerat alias quia! Nam deleniti, asperiores assumenda quia blanditiis fugiat velit eum. Est, explicabo dolores expedita labore pariatur architecto inventore nisi nihil earum libero delectus fugit ratione nostrum ullam temporibus tempora cum quasi dolor deleniti minima. Excepturi nemo est velit quaerat incidunt et, aperiam dignissimos, similique distinctio quasi non.</p>
+        </div>
+    );
+};
+
+export default About;
