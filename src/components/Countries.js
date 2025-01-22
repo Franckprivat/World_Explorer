f28c3aeb8deaf187acc19defd67f7de65b670dda
Franck kiemde <kiemdefranck5@gmail.com>
2025-01-22 21:00:43 +0100

    modif

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -2,32 +2,18 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
 const Countries = () => {
-  const [countries, setCountries] = useState([]);
-  const [error, setError] = useState(null);
+//  const [data, setData] = useState([])
+    // Le useEffect se joue lorsque le composant est monté
+    useEffect(() => {
+        axios.get("https://restcountries.com/v3.1/all")
+        .then((res) => console.log(res.data));
+    }, []);
 
-  useEffect(() => {
-    axios.get("https://restcountries.com/v3.1/all")
-      .then(res => {
-        setCountries(res.data);
-      })
-      .catch(error => {
-        setError(error.message);
-      });
-  }, []);
-
-  return (
-    <div className='countries'>
-      <h1>COUNTRIES</h1>
-      {error && <p>Une erreur s'est produite : {error}</p>}
-      {countries.length > 0 && (
-        <ul>
-          {countries.map(country => (
-            <li key={country.name.common}>{country.name.common}</li>
-          ))}
-        </ul>
-      )}
-    </div>
-  );
+    return (
+        <div className='countries'>
+            <h1>COUNTRIES</h1>
+        </div>
+    );
 };
 
 export default Countries;
