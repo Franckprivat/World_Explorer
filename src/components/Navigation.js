0c497f72d2edf834f04acec18c4b0628faa775bb
Franck kiemde <kiemdefranck5@gmail.com>
2025-01-27 18:56:19 +0100

    modif

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,10 +6,13 @@ const Navigation = () => {
     <div className="navigation">
       <ul>
         <NavLink to="/" className={(nav) => (nav.isActive ? "nav-active" : "")}>
-          <li>accueil</li>
+          <li>Accueil</li>
         </NavLink>
-        <NavLink to="/about" className={(nav) => (nav.isActive ? "nav-active" : "")}>
-          <li>à propos</li>
+        <NavLink
+          to="/about"
+          className={(nav) => (nav.isActive ? "nav-active" : "")}
+        >
+          <li>A propos</li>
         </NavLink>
       </ul>
     </div>
